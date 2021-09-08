48c6f26e0e80392c88581edc6b6c43202dcd1890
Anna Vasileva <anette.vasileva@gmail.com>
2021-09-08 11:39:02 +0300

    применяет debounce, убирает комментарии

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import fetchCountries from './fetchCountries';
 import listCountries from './templates/list-countries.hbs';
 import oneCountry from './templates/one-country.hbs';
 
+import debounce from 'lodash.debounce';
+
 const inputEl = document.querySelector('.country__search');
 const boxCountryEl = document.querySelector('.country__list');
 
-inputEl.addEventListener('input', foundCountries);
+inputEl.addEventListener('input', debounce(foundCountries, 500));
 
 function foundCountries(e) {
   const name = e.target.value;
@@ -20,4 +22,8 @@ function foundCountries(e) {
       boxCountryEl.innerHTML = countries;
     }
   });
+  // .catch(error => {
+  //   console.log(`ошибочка ${error}`);
+  //   boxCountryEl.innerHTML = '';
+  // });
 }
