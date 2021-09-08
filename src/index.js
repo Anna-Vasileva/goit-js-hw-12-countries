679b4330922d471782298b9c36093f029c2bbba0
Anna Vasileva <anette.vasileva@gmail.com>
2021-09-08 21:05:01 +0300

    подключает pnotify

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import './sass/main.scss';
+import '@pnotify/core/dist/BrightTheme.css';
+import { error, Stack } from '@pnotify/core';
+
 import fetchCountries from './fetchCountries';
 
 import listCountries from './templates/list-countries.hbs';
@@ -8,6 +11,21 @@ import debounce from 'lodash.debounce';
 
 const inputEl = document.querySelector('.country__search');
 const boxCountryEl = document.querySelector('.country__list');
+const textSpecific = 'Сделайте запрос более специфичным!';
+const textNotFound = 'Ничего не найдено!';
+
+const myError = text => {
+  error({
+    text: text,
+    delay: 500,
+    sticker: false,
+    closer: false,
+    stack: new Stack({
+      dir1: 'down',
+      dir2: 'left',
+    }),
+  });
+};
 
 inputEl.addEventListener('input', debounce(foundCountries, 500));
 
@@ -18,20 +36,21 @@ function foundCountries(e) {
     .then(country => {
       //   console.log(country);
       if (country.length > 10) {
+        clearList();
         console.log('сделать запрос более специфичным');
+        myError(textSpecific);
       } else {
         const countries = country.length < 2 ? oneCountry(country) : listCountries(country);
         boxCountryEl.innerHTML = countries;
       }
     })
     .catch(error => {
-      //   console.log(`ошибочка`);
       console.log(error);
-      notFound();
-      //   boxCountryEl.innerHTML = '';
+      clearList();
+      myError(textNotFound);
     });
 }
 
-function notFound() {
+function clearList() {
   boxCountryEl.innerHTML = '';
 }
